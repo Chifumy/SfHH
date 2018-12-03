3e8fb3b1224532548de09671a630cf682a58d2df
Leeloo <37037687+Chifumy@users.noreply.github.com>
2018-12-03 18:22:19 +0100

    Update ggi.user.js

--- G.G.I/ggi.user.js
+++ G.G.I/ggi.user.js
@@ -31,7 +31,7 @@ const init= ()=> {
 const run= ()=> {
 	let path= window.location.href;
 	let id= window.location.pathname.split('/').reverse().filter(a=>{return!!a})[0];
-	for(let a=0;a<5;a++) {
+	for(let a=0;a<6;a++) {
         $('body > div.ava').append(`<a href="${path}ava${a}.png" download="${id}.ava.${a}.png"><img src="${path}ava${a}.png"/></a>`);
         $('body > div.ico').append(`<a href="${path}ico${a}.png" download="${id}.ico.${a}.png"><img src="${path}ico${a}.png"/></a>`);
     }
